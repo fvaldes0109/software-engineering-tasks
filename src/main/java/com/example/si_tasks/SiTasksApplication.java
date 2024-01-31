b51f45cc376ee38ca8f9134c4d0257ffe10c0bb5
fvaldes0109 <fvaldes0109@gmail.com>
2024-01-31 18:54:39 +0100

    First commit

---
+++ src/main/java/com/example/si_tasks/SiTasksApplication.java
@@ -0,0 +1,12 @@
+package com.example.si_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiTasksApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/si_tasks/SiTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.si_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
